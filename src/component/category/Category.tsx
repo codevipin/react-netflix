ec8b1048783099e3594bd7d24708255fbd07ab50
vipin.verma <vipin.verma@chorus.ai>
2020-09-27 17:18:28 -0400

    [master] replaced classed component with functional and used useState and useEffect

--- src/component/category/Category.tsx
+++ src/component/category/Category.tsx
@@ -1,47 +1,45 @@
-import React, { Component } from "react";
+import React from "react";
 import ContentList from "../content-list/ContentList";
 import { CategoryData } from "../../constants/category.constant";
 import { Genre } from "../../models/category.model";
-export class Category extends Component {
-  render() {
-    return (
-      <div>
-        <ContentList
-          title={CategoryData[Genre.ORIGINAL].title}
-          url={CategoryData[Genre.ORIGINAL].subUrl}
-        />
-        <ContentList
-          title={CategoryData[Genre.TRENDING].title}
-          url={CategoryData[Genre.TRENDING].subUrl}
-          extraData={CategoryData[Genre.TRENDING].extraData}
-        />
-        <ContentList
-          title={CategoryData[Genre.TOP_RATED].title}
-          url={CategoryData[Genre.TOP_RATED].subUrl}
-        />
-        <ContentList
-          title={CategoryData[Genre.ACTION].title}
-          url={CategoryData[Genre.ACTION].subUrl}
-          extraData={CategoryData[Genre.ACTION].extraData}
-        />
-        <ContentList
-          title={CategoryData[Genre.COMEDY].title}
-          url={CategoryData[Genre.COMEDY].subUrl}
-          extraData={CategoryData[Genre.COMEDY].extraData}
-        />
-        <ContentList
-          title={CategoryData[Genre.HORROR].title}
-          url={CategoryData[Genre.HORROR].subUrl}
-          extraData={CategoryData[Genre.HORROR].extraData}
-        />
-        <ContentList
-          title={CategoryData[Genre.DOCUMENTRY].title}
-          url={CategoryData[Genre.DOCUMENTRY].subUrl}
-          extraData={CategoryData[Genre.DOCUMENTRY].extraData}
-        />
-      </div>
-    );
-  }
-}
+export const Category = () => {
+  return (
+    <div>
+      <ContentList
+        title={CategoryData[Genre.ORIGINAL].title}
+        url={CategoryData[Genre.ORIGINAL].subUrl}
+      />
+      <ContentList
+        title={CategoryData[Genre.TRENDING].title}
+        url={CategoryData[Genre.TRENDING].subUrl}
+        extraData={CategoryData[Genre.TRENDING].extraData}
+      />
+      <ContentList
+        title={CategoryData[Genre.TOP_RATED].title}
+        url={CategoryData[Genre.TOP_RATED].subUrl}
+      />
+      <ContentList
+        title={CategoryData[Genre.ACTION].title}
+        url={CategoryData[Genre.ACTION].subUrl}
+        extraData={CategoryData[Genre.ACTION].extraData}
+      />
+      <ContentList
+        title={CategoryData[Genre.COMEDY].title}
+        url={CategoryData[Genre.COMEDY].subUrl}
+        extraData={CategoryData[Genre.COMEDY].extraData}
+      />
+      <ContentList
+        title={CategoryData[Genre.HORROR].title}
+        url={CategoryData[Genre.HORROR].subUrl}
+        extraData={CategoryData[Genre.HORROR].extraData}
+      />
+      <ContentList
+        title={CategoryData[Genre.DOCUMENTRY].title}
+        url={CategoryData[Genre.DOCUMENTRY].subUrl}
+        extraData={CategoryData[Genre.DOCUMENTRY].extraData}
+      />
+    </div>
+  );
+};
 
 export default Category;
--- src/component/content-list/ContentList.tsx
+++ src/component/content-list/ContentList.tsx
@@ -1,52 +1,38 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import "./ContentList.scss";
 import NfOriginalCard from "../nf-original-card/NfOriginalCard";
-import IContentListProps, {
-  IContentData,
-} from "../../models/content-list.model";
+import IContentListProps, { IContent } from "../../models/content-list.model";
 import { contentService } from "../../services/content.service";
 import { CategoryData } from "../../constants/category.constant";
 import { Genre } from "../../models/category.model";
-export class ContentList extends Component<IContentListProps, IContentData> {
-  constructor(props: IContentListProps) {
-    super(props);
-    this.state = {
-      results: [],
-      total_results: 0,
-    };
-  }
-  componentDidMount() {
-    const onSuccess = (response: IContentData) => {
-      console.log("response", response);
-      this.setState(response);
-    };
-    const onError = () => {
-      console.log("error occured while fetch data for ", this.props.url);
-    };
-    contentService
-      .fetchContent(this.props.url, this.props.extraData)
-      .then(onSuccess, onError);
-  }
-  render() {
-    return (
-      <div className="content-list">
-        <h2 className="content-title color-white margin-0">
-          {this.props.title}
-        </h2>
-        <div className="content-items flex">
-          {this.state.results.map((content) => (
-            <NfOriginalCard
-              key={content.id}
-              content={content}
-              isImagePortrait={
-                this.props.title === CategoryData[Genre.ORIGINAL].title
-              }
-            />
-          ))}
-        </div>
+
+function ContentList(props: IContentListProps) {
+  const [movies, setMovies]: [IContent[], Function] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const data = await contentService.fetchContent(
+        props.url,
+        props.extraData
+      );
+      setMovies(data.results);
+      return data;
+    }
+    fetchData();
+  }, [props]);
+  return (
+    <div className="content-list">
+      <h2 className="content-title color-white margin-0">{props.title}</h2>
+      <div className="content-items flex">
+        {movies.map((movie) => (
+          <NfOriginalCard
+            key={movie.id}
+            content={movie}
+            isImagePortrait={props.title === CategoryData[Genre.ORIGINAL].title}
+          />
+        ))}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default ContentList;
