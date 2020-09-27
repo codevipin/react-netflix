934dc1d6ba93b76b86cb77a22681a7843819d072
vipin.verma <vipin.verma@chorus.ai>
2020-09-26 21:54:18 -0400

    [master] added build folder

---
+++ build/precache-manifest.e6baf36bbee8b00690c119d9618e5b2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "249db0791a67a22679e5a0b1cc9972a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea2563dd489b0e85ff83",
+    "url": "/static/css/main.c6cbac59.chunk.css"
+  },
+  {
+    "revision": "85fa20792bfb34b337a8",
+    "url": "/static/js/2.8d8664d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8d8664d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea2563dd489b0e85ff83",
+    "url": "/static/js/main.11ba6164.chunk.js"
+  },
+  {
+    "revision": "fdf1b30335a5e51d8dad",
+    "url": "/static/js/runtime-main.60e15acb.js"
+  }
+]);
